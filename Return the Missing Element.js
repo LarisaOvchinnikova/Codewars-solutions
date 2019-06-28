c0a26d2689044cd82095fc68d062dc971741ea03
Larisa <larisaplantation@gmail.com>
2019-06-28 11:46:07 -0400

    Added file 'Return the Missing Element' Renew file readme

---
+++ Return the Missing Element.js
@@ -0,0 +1,8 @@
+/* Return the Missing Element
+(https://www.codewars.com/kata/5299413901337c637e000004)
+Solution*/
+	function getMissingElement(superImportantArray){
+    for (let i = 0; i <= 9; i++){
+     if (!superImportantArray.includes(i)) return i;
+     }
+    }
