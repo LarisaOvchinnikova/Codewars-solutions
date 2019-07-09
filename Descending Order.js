a6762ae53cc163c9ab272e03a0b4777e8cd5614b
Larisa <larisaplantation@gmail.com>
2019-07-09 18:13:45 -0400

    added file 'Descending Order'

---
+++ Descending Order.js
@@ -0,0 +1,7 @@
+/*
+Descending Order
+https://www.codewars.com/users/LarisaOvchinnikova/completed_solutions
+*/
+function descendingOrder(n){
+  return +n.toString().split('').sort().reverse().join('');
+}
