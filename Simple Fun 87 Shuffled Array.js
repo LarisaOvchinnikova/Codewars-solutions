358eb912eaa14df913320fc1d0e2a75bc6099505
Larisa <larisaplantation@gmail.com>
2020-01-13 20:06:17 -0500

    added file

--- Simple Fun 87 Shuffled Array.js
+++ Simple Fun 87 Shuffled Array.js
@@ -1,5 +1,5 @@
 //Simple Fun #87: Shuffled Array
-//
+//https://www.codewars.com/kata/589573e3f0902e8919000109
 function shuffledArray(arr) {
   let sum = arr.reduce((a,b) => a + b);
   let el = sum / 2;
