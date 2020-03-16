e1bd83c01e30f1696955e766a8d6c792125a9ae3
Larisa <larisaplantation@gmail.com>
2020-03-16 17:04:40 -0400

    added file

---
+++ Number-Star ladder.js
@@ -0,0 +1,11 @@
+//Number-Star ladder
+//https://www.codewars.com/kata/5631213916d70a0979000066
+
+function pattern(n){
+ let output="1\n";
+ for (let i = 1; i < n-1; i++){
+    output = output + '1' + '*'.repeat(i) + (i+1) + '\n'
+ }
+ output = output + '1' + '*'.repeat(n-1) + n;
+ return output;
+}
