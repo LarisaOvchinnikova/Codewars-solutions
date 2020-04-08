56ad95d4639f7229d7d65c2432fd1786abbe7f5f
Larisa <larisaplantation@gmail.com>
2020-04-08 14:56:23 -0400

    added solution Greet developers

---
+++ Higher-Order Functions Series - Greet developers.js
@@ -0,0 +1,9 @@
+//  Coding Meetup #2 - Higher-Order Functions Series - Greet developers
+// https://www.codewars.com/kata/58279e13c983ca4a2a00002a/train/javascript
+
+function greetDevelopers(list) {
+for (let i in list){
+  list[i].greeting = `Hi ${list[i].firstName}, what do you like the most about ${list[i].language}?`;
+}
+return list
+}
