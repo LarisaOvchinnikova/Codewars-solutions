32342d37c3d626a7b255b36902fd45d4d607ee1c
Larisa Ovchinnikova <51036786+LarisaOvchinnikova@users.noreply.github.com>
2021-02-04 22:28:06 -0500

    Update Covfefe.js

--- Covfefe.js
+++ Covfefe.js
@@ -1,5 +1,5 @@
 //Covfefe
-// https://www.codew
+// https://www.codewars.com/kata/592fd8f752ee71ac7e00008a
 function covfefe(str){
   return (str.includes('coverage')) ? str.replace(/coverage/gi,'covfefe') : str +' covfefe';
  }
